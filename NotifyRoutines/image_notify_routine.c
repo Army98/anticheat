4775e808103618cb64d07916118b276b4bb05447
Army Khajkiani <Armykhajkiani1@gmail.com>
2025-09-26 03:01:23 -0700

    check this shit is dumb i hate github

--- NotifyRoutines/image_notify_routine.c
+++ NotifyRoutines/image_notify_routine.c
@@ -6,7 +6,7 @@ VOID ImageNotifyRoutine(PUNICODE_STRING FullImageName, HANDLE ProcessID, PIMAGE_
 {
 	/*
 	* detect signed vs unsigned drivers, known-bad driver names and their hashes
-	* detect known cheat DLLs, exe's, unusual DLLs mapped into game processes blah
+	* detect known cheat DLLs, exe's, unusual DLLs mapped into game processes blah fegergr
 	*/
 	UNREFERENCED_PARAMETER(FullImageName);
 	UNREFERENCED_PARAMETER(ProcessID);
